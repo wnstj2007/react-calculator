dfa5329658b3b79397ef573f7d01ca870b4215a8
wnstj2007 <wnstj0207@naver.com>
2020-06-13 03:24:30 +0900

    Updates

---
+++ precache-manifest.cd2ea40d03cef157a36c7d59f51a72d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13ff14fae94ca793ec4c",
+    "url": "/react-calculator/static/css/main.61be3fee.chunk.css"
+  },
+  {
+    "revision": "13ff14fae94ca793ec4c",
+    "url": "/react-calculator/static/js/main.13ff14fa.chunk.js"
+  },
+  {
+    "revision": "1a3e195fbf1f59107b1b",
+    "url": "/react-calculator/static/css/1.a3323fb9.chunk.css"
+  },
+  {
+    "revision": "1a3e195fbf1f59107b1b",
+    "url": "/react-calculator/static/js/1.1a3e195f.chunk.js"
+  },
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "d951b9d2f6d8d511b02e35da19c0dc24",
+    "url": "/react-calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-calculator/precache-manifest.72f01110fbb96038031279d4d11eea00.js"
+  "/react-calculator/precache-manifest.cd2ea40d03cef157a36c7d59f51a72d2.js"
 );
 
 workbox.clientsClaim();
